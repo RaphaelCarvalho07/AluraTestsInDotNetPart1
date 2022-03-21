8ecaa1081334ff6d3e52fb35687f09476bc1d325
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-21 19:09:10 -0300

    Aula 03.Testando exceções - 05.Testando Exceções - Alura Tests In CSharp Part 1

--- Alura.Estacionamento.Testes/VeiculoTeste.cs
+++ Alura.Estacionamento.Testes/VeiculoTeste.cs
@@ -70,6 +70,36 @@ namespace Alura.Estacionamento.Testes
             Assert.Contains( "Ficha do Veículo:", dados );
         }
 
+        [Fact]
+        public void TesteNomeProprietarioVeiculoComMenosDeTresCaracteres()
+        {
+            // Arrange
+            string nomeProprietario = "Ab";
+
+            // Assert
+            Assert.Throws<System.FormatException>
+                (
+                    // Act
+                    () => new Veiculo( nomeProprietario )
+                );
+        }
+
+        [Fact]
+        public void TestaMensagemExcecaoDoQuartoCaracterDaPlaca()
+        {
+            // Arrange
+            string placa = "ASDF8888";
+
+            // Act
+            var mensagem = Assert.Throws<System.FormatException>
+            (
+                () => new Veiculo().Placa = placa
+            );
+
+            // Assert
+            Assert.Equal( "O 4° caractere deve ser um hífen", mensagem.Message );
+        }
+
         public void Dispose()
         {
             SaidaConsoleTeste.WriteLine( "Dispose invocado." );
--- Alura.Estacionamento/Alura.Estacionamento.Modelos/Veiculo.cs
+++ Alura.Estacionamento/Alura.Estacionamento.Modelos/Veiculo.cs
@@ -67,7 +67,18 @@ namespace Alura.Estacionamento.Modelos
         public string Modelo { get; set; }
         public string Proprietario
         {
-            get; set;
+            get
+            {
+                return _proprietario;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new System.FormatException( " Nome do proprietário deve ter no mínimo 3 caracteres." );
+                }
+                _proprietario = value;
+            }
         }
         public DateTime HoraEntrada { get; set; }
         public DateTime HoraSaida { get; set; }
@@ -122,7 +133,7 @@ namespace Alura.Estacionamento.Modelos
 
         public override string ToString()
         {
-            return  $"Ficha do Veículo:\n " +
+            return $"Ficha do Veículo:\n " +
                     $"Tipo do Veículo: { this.Tipo.ToString()}\n" +
                     $"Proprietário: {this.Proprietario}\n" +
                     $"Modelo: {this.Modelo}\n" +
